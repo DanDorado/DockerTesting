8ff8a33bf9354ad11fe4625d7d71ffff4678c5e7
Dan Joyce <Dan.Joyce@uk.fleetcor.com>
2020-10-22 13:19:48 +0100

    test live change

--- sayhello.py
+++ sayhello.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 def hello_world():
     user = os.environ["USER"]
     return("Hello, folks. My random number is " +
-    str(random_number(10)) + ". \n\n\n The username pulled from .env is " + str(user))
+    str(random_number(1000)) + ". \n\n\n The username pulled from .env is " + str(user))
 
 def random_number(max):
-    return(random.randint(0,max))
+    return(random.randint(100,max))
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port=1000, debug = True)
